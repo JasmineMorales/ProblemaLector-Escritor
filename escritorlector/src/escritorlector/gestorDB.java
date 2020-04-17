076761ba2a4dcfbb918c04f106def3b77b74c030
Aliciarroyace <aliciarroyave1022019@gmial.com>
2020-04-17 09:29:51 -0600

    LECTOR Y ESCRITOR

---
+++ escritorlector/src/escritorlector/gestorDB.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package escritorlector;
+
+/**
+ *
+ * @author Jonathan
+ */
+public class gestorDB {
+
+    void openE(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void closeE(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void openL(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void closeL(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
