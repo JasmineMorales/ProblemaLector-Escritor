0f3951344c168311970d4cb1f04c47004d374c32
Aludor <manuelvillagran@hotmail.com>
2020-04-18 13:58:44 -0600

    Lectores

--- escritorlector/src/escritorlector/gestorDB.java
+++ escritorlector/src/escritorlector/gestorDB.java
@@ -10,20 +10,29 @@ package escritorlector;
  *
  */
 public class gestorDB {
+    private int nLectores = 0;
+    private boolean hayEscritor = false;
+    private int nEscritor = 0;
+
+    void openL(int id) throws InterruptedException{
+        while(hayEscritor || nEscritor > 0){
+            wait();
+        }
+        nLectores++;
+        System.out.println("Lector " + id + "entro a la BD");
+    }
+
+    void closeL(int id) {
+        System.out.println("Lector " + id + "salio de la BD");
+        nLectores--;
+        if(nLectores == 0 ) notifyAll();
+    }
 
     void openE(int id) throws InterruptedException{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     void closeE(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    void openL(int id) throws InterruptedException{
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    void closeL(int id) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
